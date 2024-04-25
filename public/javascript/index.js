801e1b3a582c897f64870befb6373dae7267e79f
Nilton Silva <165297557+j-nilton@users.noreply.github.com>
2024-04-25 20:44:36 -0300

    Upload files

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,18 +1,16 @@
-function dizerOi():void{
-    let meuTitulo : HTMLElement | null;
+"use strict";
+function dizerOi() {
+    let meuTitulo;
     meuTitulo = document.getElementById('titulo-principal');
-    meuTitulo!.innerHTML = 'Oi, José Nilton!';
+    meuTitulo.innerHTML = 'Oi, José Nilton!';
     /*if(meuTitulo!.innerHTML == 'Oi, José Nilton!'){
         meuTitulo!.innerHTML = 'Você é ele mesmo?';
     }else{
         meuTitulo!.innerHTML = 'Oi, José Nilton!';
     }*/
-
-    let meuInput : HTMLElement | null;
+    let meuInput;
     meuInput = document.getElementById('campo-texto');
-
-    let entrada : string;
-    entrada = meuInput!.value;
-
-    meuTitulo!.innerHTML = entrada;
+    let entrada;
+    entrada = meuInput.value;
+    meuTitulo.innerHTML = entrada;
 }
