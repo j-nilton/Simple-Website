7b62ba3765edab9cb04b650c3b8587d728303259
Nilton Silva <165297557+j-nilton@users.noreply.github.com>
2024-04-30 20:39:51 -0300

    Upload files

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,24 @@
 $('.slider-principal').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000
-  });
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000
+});
+
+const btnExpandir = document.getElementById('btnExpandir');
+const abaExpandida = document.getElementById('abaExpandida');
+
+btnExpandir.addEventListener('click', () => {
+  if (abaExpandida.classList.contains('hidden')) {
+    abaExpandida.classList.remove('hidden');
+    btnExpandir.textContent = '-';
+  } else {
+    abaExpandida.classList.add('hidden');
+    btnExpandir.textContent = '+';
+  }
+});
+
+
+
